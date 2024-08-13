db11d1648f24a708dce3d02502ff69cafc63408f
Godwin <godwinchuks032@gmail.com>
2024-08-13 13:11:10 +0100

    Upadted the middleware to access the end-point of th sign-in/sign-up and design the sign ip/up page with tailwind css

--- fintracker/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ fintracker/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,30 @@
-import { SignIn } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
 
 export default function Page() {
-    return (<SignIn />);
-};
+    return (
+        <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+            <div className="h-full lg:flex flex-col items-center justify-center px-4">
+                <div className="text-center space-y-4 pt-6">
+                    <h1 className="font-bold text-3xl text-[#53565B]">
+                        Welcome Back
+                    </h1>
+                    <p className="text-base text-[#5988C5]">
+                        Sign in or create an account to access your dashboard!
+                    </p>
+                </div>
+                <div className="flex items-center justify-center mt-8">
+                    <ClerkLoaded>
+                        <SignIn />
+                    </ClerkLoaded>
+                    <ClerkLoading>
+                        <Loader2 className="animate-spin text-muted-foreground"/>
+                    </ClerkLoading>
+                </div>
+            </div>
+            <div className="h-full bg-gray-400 hidden lg:flex items-center justify-center">
+
+            </div>
+        </div>
+);
+}
