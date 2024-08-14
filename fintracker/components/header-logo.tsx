61a232d27b38675098032013afca3c76eebedefb
Godwin <godwinchuks032@gmail.com>
2024-08-14 22:31:06 +0100

    Created nagivation buttons for the front page, modified the headers components

--- fintracker/components/header-logo.tsx
+++ fintracker/components/header-logo.tsx
@@ -4,8 +4,11 @@ import Link from "next/link"
 export const HeaderLogo = () => {
     return (
         <Link href="/">
-            <div>
-                Logo
+            <div className="items-center hidden lg:flex">
+                <Image src="/image.jpg" alt="image" height={28} width={28} />
+                <p className="font-semibold text-white text-2xl ml-2.5">
+                    Financial Tracker
+                </p>
             </div>
         </Link>
     );
