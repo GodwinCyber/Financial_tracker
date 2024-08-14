e0d227f2c75ca481a1db27cc5273b7ce57355ad3
Godwin <godwinchuks032@gmail.com>
2024-08-14 08:14:20 +0100

    Creating a front end component for the layout

---
+++ fintracker/components/header-logo.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image"
+import Link from "next/link"
+
+export const HeaderLogo = () => {
+    return (
+        <Link href="/">
+            <div>
+                Logo
+            </div>
+        </Link>
+    );
+}
