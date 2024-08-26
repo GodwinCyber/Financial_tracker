57b1aa276214f226fec708901ca452f63ca3359b
Godwin <godwinchuks032@gmail.com>
2024-08-26 09:36:36 +0300

    upload csv file

--- fintracker/app/(dashboard)/transactions/page.tsx
+++ fintracker/app/(dashboard)/transactions/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 
+import { UploadButton } from "./upload-button";
+import { ImportCard } from "./import-card";
+
 import { useGetTransactions } from "@/features/transactions/api/use-get-transactions";
 import { useNewTransaction } from "@/features/transactions/hooks/use-new-transaction";
 import { useBulkDeleteTransactions } from "@/features/transactions/api/use-bulk-delete-transactions";
 
 import { Loader2, Plus } from "lucide-react";
 import { columns } from "./columns";
+import { useState } from "react";
+
 
 import { DataTable } from "@/components/data-table";
 import { Skeleton } from "@/components/ui/skeleton";
@@ -17,8 +22,33 @@ import {
     CardContent
 } from "@/components/ui/card";
 
+enum VARIANTS {
+    LIST = "LIST",
+    IMPORT = "IMPORT"
+};
+
+const INITIAL_IMPORT_RESULTS = {
+    data: [],
+    error: [],
+    meta: {},
+};
+
 
 const TransactionsPage = () => {
+    const [variant, setVariant] = useState<VARIANTS>(VARIANTS.LIST);
+    const [importResults, setImportResults] = useState(INITIAL_IMPORT_RESULTS);
+
+    const onUpload = (results: typeof INITIAL_IMPORT_RESULTS) => {
+        console.log({ results });
+        setImportResults(results);
+        setVariant(VARIANTS.IMPORT);
+    };
+
+    const onCancelImport = () => {
+        setImportResults(INITIAL_IMPORT_RESULTS);
+        setVariant(VARIANTS.LIST);
+    }
+
     const newTransaction = useNewTransaction();
     const deleteTransactions = useBulkDeleteTransactions();
     const transactionsQuery = useGetTransactions();
@@ -40,8 +70,21 @@ const TransactionsPage = () => {
                     </CardContent>
                 </Card>
             </div>
-        )
+        );
     }
+
+    if (variant === VARIANTS.IMPORT) {
+        return (
+            <>
+                <ImportCard 
+                  data={importResults.data}
+                  onCancel={onCancelImport}
+                  onSubmit={() => {}} 
+                />
+            </>
+        );
+    }
+
     return (
         <div className="max-screen-2xl mx-auto w-full pb-10 -mt-24">
             <Card className="border-none drop-shadow-sm">
@@ -49,10 +92,13 @@ const TransactionsPage = () => {
                     <CardTitle className="text-xl line-clamp-1">
                         Transaction History
                     </CardTitle>
-                    <Button onClick={newTransaction.onOpen} size="sm">
-                        <Plus className="size-4 mr-2"/>
-                        Add New 
-                    </Button>
+                    <div className="flex items-center gap-x-2">
+                        <Button onClick={newTransaction.onOpen} size="sm">
+                            <Plus className="size-4 mr-2"/>
+                            Add New 
+                        </Button>
+                        <UploadButton onUpload={onUpload}/>
+                    </div>
                 </CardHeader>
                 <CardContent>
                     <DataTable
